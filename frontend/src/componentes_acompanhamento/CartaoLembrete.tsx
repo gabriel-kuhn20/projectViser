898b63a521cf0c5f484e79f6f26cfaea2f9eceef
Gabriel <gabrieldefranca03@gmail.com>
2026-09-21 23:32:11 -0300

    feat: atualização das pastas, deixando-a mais proxima do modelo de dados atual

--- frontend/src/componentes_acompanhamento/CartaoLembrete.tsx
+++
@@ -1,5 +0,0 @@
-// mostra um cliente pendente de contato, com ação de concluir e de
-// marcar/desmarcar "em atendimento" (UC08, UC09)
-export function CartaoLembrete() {
-  return <div />;
-}
--- frontend/src/componentes_acompanhamento/ListaAcompanhamentos.tsx
+++
@@ -1,6 +0,0 @@
-import { CartaoLembrete } from "./CartaoLembrete";
-
-export function ListaLembretes() {
-  // TODO: buscar lembretes pendentes via ApiLembrete, por marco selecionado
-  return <div />;
-}
